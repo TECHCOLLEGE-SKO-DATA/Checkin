28f8dcb8a8756a72e01f47faef9393fcc92c60c4
Casper S Jensen <caspersjensen@outlook.dk>
2025-04-07 14:58:56 +0200

    Avalonia Version of FakeNFC added #71

---
+++ CheckinSystemAvalonia/Views/Windows/FakeNFCWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CheckinSystemAvalonia;
+
+public partial class FakeNFCWindow : Window
+{
+    public FakeNFCWindow()
+    {
+        InitializeComponent();
+    }
+}
