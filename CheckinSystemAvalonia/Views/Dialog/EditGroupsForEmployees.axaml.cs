c05838fdcabe0f191e990523ed7acf9165d4ea3b
Casper S Jensen <caspersjensen@outlook.dk>
2025-04-10 08:27:58 +0200

    Added More dialog windows from the WPF version to avalonia

---
+++ CheckinSystemAvalonia/Views/Dialog/EditGroupsForEmployees.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CheckinSystemAvalonia;
+
+public partial class EditGroupsForEmployees : Window
+{
+    public EditGroupsForEmployees()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CheckinSystemAvalonia/Views/Dialog/InputDialog.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CheckinSystemAvalonia;
+
+public partial class InputDialog : Window
+{
+    public InputDialog()
+    {
+        InitializeComponent();
+    }
+}
