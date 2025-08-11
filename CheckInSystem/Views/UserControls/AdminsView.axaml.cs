92e023f7823659acdede68a953c73f4b7b4012b8
Casper S Jensen <caspersjensen@outlook.dk>
2025-08-11 09:06:48 +0200

    Added usercontrol for adding and managing  admins #83

---
+++ CheckInSystem/Views/UserControls/AdminsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CheckInSystem;
+
+public partial class AdminsView : UserControl
+{
+    public AdminsView()
+    {
+        InitializeComponent();
+    }
+}
