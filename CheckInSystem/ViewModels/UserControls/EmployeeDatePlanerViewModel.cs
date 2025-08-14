aa1fa495892dcc48c55e6db66988c1573ce40d6d
Casper S Jensen <caspersjensen@outlook.dk>
2025-08-14 14:18:35 +0200

    Making alternative GUI for EmployeeTime to possible make it easier to understand #80

---
+++ CheckInSystem/ViewModels/UserControls/EmployeeDatePlanerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CheckInSystem.Platform;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckInSystem.ViewModels.UserControls
+{
+    public class EmployeeDatePlanerViewModel : ViewModelBase
+    {
+        public ReactiveCommand<Unit, Unit> Btn_Back { get; }
+
+        public ReactiveCommand<Unit, Unit> Btn_Logout { get; }
+        public EmployeeDatePlanerViewModel(IPlatform platform) : base(platform) 
+        { 
+            Btn_Back = ReactiveCommand.Create(() => _platform.MainWindowViewModel.SwitchToAdminPanel());
+            Btn_Logout = ReactiveCommand.Create(() => _platform.MainWindowViewModel.SwitchToLoginView());
+        }  
+    }
+}
