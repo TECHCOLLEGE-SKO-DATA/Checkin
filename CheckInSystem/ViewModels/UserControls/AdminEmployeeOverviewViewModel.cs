cb51324e1b0a240244aab64373cf4a087f436a74
Casper S Jensen <caspersjensen@outlook.dk>
2025-09-11 09:10:00 +0200

    added theoretical UI for seeing where everybody is on what day(not functional yet) #80

---
+++ CheckInSystem/ViewModels/UserControls/AdminEmployeeOverviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CheckInSystem.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckInSystem.ViewModels.UserControls
+{
+    public class AdminEmployeeOverviewViewModel : ViewModelBase
+    {
+        public AdminEmployeeOverviewViewModel(IPlatform platform):base(platform) 
+        {
+            
+        }
+    }
+}
---
+++ CheckInSystem/Views/UserControls/AdminEmployeeOverviewView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CheckInSystem;
+
+public partial class AdminEmployeeOverviewView : UserControl
+{
+    public AdminEmployeeOverviewView()
+    {
+        InitializeComponent();
+    }
+}
